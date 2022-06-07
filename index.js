7cfed43c53dd3e724be2a6d9ba2e97ca197cf799
Kipruto Duncan <kiprutoduncan36@gmail.com>
2022-06-07 18:22:27 +0300

    commit tree 88e0c07ca8b06fce138c52855d9247134fdd411f

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "string".toUpperCase
+  "Hello!".toUpperCase();
+  function whisper(string){
+      return string.toLowerCase()
+  }
+  "string".toLowerCase()
+  function logShout(string){
+      console.log("hello".toUpperCase())
+  }
+console.log(logShout(string))   
+function logWhisper(string){
+    console.log("hello".toLowerCase())
+}
+console.log(logWhisper(string))
+
+function sayHiToHeadPhonedRoomate (string) {
+    string.toLowerCase(string)===lowercase;
+    if ( lowercase=== string) {
+      console.log("I can't hear you!");
+    }
+    else if (string.toUpperCase(string) === string) {
+      return "YES INDEED!";
+    }
+    else if ("I would love to!" === string) {
+      return "I would love to!"
+    }
+  }
+  console.log(string==string.toLowerCase(string))
